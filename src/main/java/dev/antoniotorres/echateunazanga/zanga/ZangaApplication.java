d887cae736e68e330adab3d7fa72418294ef6b6f
antoniotorres02 <torresbordonantonio@gmail.com>
2024-09-15 14:46:35 +0200

    FEAT: Initialized Backend Project

---
+++ src/main/java/dev/antoniotorres/echateunazanga/zanga/ZangaApplication.java
@@ -0,0 +1,13 @@
+package dev.antoniotorres.echateunazanga.zanga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZangaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZangaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/antoniotorres/echateunazanga/zanga/ZangaApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.antoniotorres.echateunazanga.zanga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZangaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
